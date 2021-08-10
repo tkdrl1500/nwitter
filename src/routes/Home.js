b79787a7ab2703df8ccc3e1c32ce60f570b9e446
tkdrl1500 <tkdrl1500@gmail.com>
2021-08-11 01:33:55 +0900

    Firestore에 컬렉션 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
+import { dbService } from 'fbase';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    // 파이어스토어에 nweets라는 컬렉션을 만들고 값들을 넣어줌
+    await dbService.collection('nweets').add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet('');
   };
 
   const onChange = (e) => {
     const {
       target: { value },
     } = e;
-    if (value === 'nweet') {
-      setNweet(value);
-    }
+    setNweet(value);
   };
 
   return (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -4,10 +4,12 @@ import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
   const history = useHistory();
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
